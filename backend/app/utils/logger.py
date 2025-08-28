7ffb8b24140c9483cab683e3e123f9b67e445c31
Poorani Senthil <poorani@Pooranis-MacBook-Pro.local>
2025-08-28 14:42:30 -0400

    Import logging class and set up logger object

--- backend/app/utils/logger.py
+++ backend/app/utils/logger.py
@@ -2,3 +2,14 @@ import logging
 import sys
 from pathlib import Path
 
+def setup_logger(name: str, log_file: str = None):
+    """Logger with file and console output"""
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        '%(acstime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+  
