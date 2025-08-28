e818e46ce38b65d319d409beb1d4803ea0d05918
Poorani Senthil <poorani@Pooranis-MacBook-Pro.local>
2025-08-28 14:45:50 -0400

    File handler added to method, returns logger

--- backend/app/utils/logger.py
+++ backend/app/utils/logger.py
@@ -12,4 +12,14 @@ def setup_logger(name: str, log_file: str = None):
         '%(acstime)s - %(name)s - %(levelname)s - %(message)s'
     )
 
-  
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    #File handler
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
