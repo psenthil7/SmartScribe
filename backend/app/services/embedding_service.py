427c44bfa4671cc7ab4dd67a5a7f61a13628baaa
Poorani Senthil <poorani@Pooranis-MacBook-Pro.local>
2025-08-28 11:38:25 -0400

    Used functions from sentence transformer to create embeddings from single text, and a list of texts

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -3,4 +3,12 @@ import numpy as np
 
 class EmbeddingService:
     def __init__(self):
-        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.model = SentenceTransformer('all-MiniLM-L6-v2') # model for embeddings
+
+    def generate_embeddings(self, text: str):
+        return self.model.encode(text).tolist() # creates embeddigns for signle text
+    
+    def generate_embeddings_batch(self, texts: list):
+        return self.model.encode(texts).tolist()  # creates embeddings for list of text
+
+    
