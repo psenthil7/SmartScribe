4169b27f99fb646503c639588e7eccc72f8d8bf7
Poorani Senthil <poorani@Pooranis-MacBook-Pro.local>
2025-08-28 02:10:57 -0400

    added more instances to note objects

--- backend/app/models/note.py
+++ backend/app/models/note.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
 
 class NoteCreate(BaseModel):
     title: str
     content: str
+    filename: Optional[str] = None
 
 class NoteResponse(BaseModel):
     id: int
     title: str
     content: str
+    filename: Optional[str] = None
+    created_at: datetime
+    
